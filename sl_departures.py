2c7ef6c621d85aed11ec56ce8e896056cfdd5992
fettgrym <15312902+fettgrym@users.noreply.github.com>
2024-03-17 16:03:56 +0100

    Add files via upload
    Some changes

--- sl_departures.py
+++ sl_departures.py
@@ -1,36 +1,40 @@
 from dateutil.parser import parse
-from datetime import time
-import json
 import requests
-from datetime import datetime
+import json
 
 def get_departures(headers, url):
-    departures_vallentuna = requests.get(url, headers=headers, timeout=5)
-    departures_data = departures_vallentuna.json()
+    departures_response = requests.get(url, headers=headers, timeout=5)
+    departures_data = departures_response.json()
     return departures_data
 
-def extract_next_departure(departures_data, destination):
-    next_departure = next((departure for departure in departures_data["departures"] if departure["destination"] == destination), None)
-    return next_departure
+def extract_upcoming_departures(departures_data, destination, num_departures=2):
+    upcoming_departures = [departure for departure in departures_data["departures"] if departure["destination"] == destination][:num_departures]
+    return upcoming_departures
 
 def format_departure_time(departure, key):
     if key in departure:
         datetime_obj = parse(departure[key])
         departure[key] = datetime_obj.time().strftime("%H:%M")
 
-def get_next_departure_info(headers, url, destination):
+def get_upcoming_departures_info(headers, url, destination, num_departures=2):
     departures_data = get_departures(headers, url)
-    next_departure = extract_next_departure(departures_data, destination)
-    if next_departure:
-        format_departure_time(next_departure, 'expected')
-        format_departure_time(next_departure, 'scheduled')
-        json_output = {"next_departure": next_departure}
-        return json_output
+    upcoming_departures = extract_upcoming_departures(departures_data, destination, num_departures)
+    for departure in upcoming_departures:
+        format_departure_time(departure, 'expected')
+        format_departure_time(departure, 'scheduled')
+    json_output = {"upcoming_departures": upcoming_departures}
+    return json_output
 
 HEADERS = {"Content-Type": "application/json"}
+
 STATION = "9626"
-VALLANTUNA_STATION = "https://transport.integration.sl.se/v1/sites/{}/departures?transport=TRAM&line=27&forecast=60".format(STATION)
+DIRECTION = "1"
+TRANSPORT = "TRAM"
+LINE = "27"
+FORECAST = "60"
 DESTINATION = "Stockholms östra"
 
-result = get_next_departure_info(HEADERS, VALLANTUNA_STATION, DESTINATION)
+API = f"https://transport.integration.sl.se/v1/sites/{STATION}/departures?transport={TRANSPORT}&direction={DIRECTION}&line={LINE}&forecast={FORECAST}"
+
+result = get_upcoming_departures_info(HEADERS, API, DESTINATION, 2)
 print(json.dumps(result))
